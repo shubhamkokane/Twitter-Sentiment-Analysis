1bf97be3516295c9528ff92412567ebf9ce2e3ce
Shubham Kokane <35104189+shubhamkokane@users.noreply.github.com>
2020-04-30 13:28:08 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import sentiment_mod as s
+
+print(s.sentiment("This movie was awesome!. The acting was great!"))
+
+print(s.sentiment("I loved the movie"))
+
+print(s.sentiment("The direction was good and the screenplay was average!"))
+
+
